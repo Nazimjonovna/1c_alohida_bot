bc169d0dda993281c21b1593c16a1765c6bf62ea
Nazimjonovna <sturayeva000@gmail.com>
2024-01-26 01:26:34 +0500

    button uchun

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ import requests
 import os
 from aiogram.types import CallbackQuery
 
-API_TOKEN = "6619844226:AAETgqwj9pMWa4RvT58Y90w-Gqxz_uzHY1U"
+API_TOKEN = "6293716593:AAH8dR8RNDf6Sv67Bjo9aPwla7zXb8-ihqk"
 
 api_url = 'http://5.182.26.180:55565/telegram/hs/hl/gd'
 login = 'Hilol'
@@ -104,7 +104,7 @@ async def uzb(message: types.Message):
             result = f"Request failed: {e}"
             await message.answer(f"{data.get('UZ')}")
 
-    @dp.message_handler(text="Qarzdorlikni tekshirish") # TODO this inlinekeyboard button
+    @dp.message_handler(text="Qarzdorlikni tekshirish") 
     async def tel1(message: types.Message):
         params = {
             "type": "debt",
@@ -152,7 +152,6 @@ async def uzb(message: types.Message):
         async def tel2(call: types.CallbackQuery, state: FSMContext):
             global messa
             messa = call.data
-            k = next(key for key, value in ta.items() if value == messa)
             if messa != "Orqaga":
                 params = {
                     "type": "debt",
@@ -161,22 +160,23 @@ async def uzb(message: types.Message):
                 response = requests.get(api_url, params=params, headers=headers)
                 if response:
                     data = response.json()
-                    if data['allsumm'] != None:
-                        global summ, contract_id
-                        contract_id = k
-                        await state.finish()
-                        await bot.send_message(message.chat.id, "Quyidagilardan tanlang: ", reply_markup=kop)
-                        # else:
-                        #     await bot.send_message(chat_id=chat_id, text = message_text)
-                    else:
-                        message_text = "Hozirda sizda qarzdorlik mavjud emas"
-                        await state.finish()
-                        await bot.send_message(chat_id=chat_id, text = message_text)
+                    print(data)
+                    for i in range(0, len(data['contracts'])):
+                        if data['contracts'][i]['contract'] == messa:
+                            contract_id = data['contracts'][i]['contract']
+                            contractsumm = data['contracts'][i]['contractsumm']
+                            contractcurrency = data['contracts'][i]['contractcurrency']
+                            contractekvivalent = data['contracts'][i]['contractekvivalent']
+                            message_text = f'Shartnoma - {contract_id}:\nQarzdorlik - {contractsumm} {contractcurrency} {contractekvivalent}'
+                            await bot.send_message(chat_id=chat_id, text = message_text,reply_markup=kop)
+                            await state.finish()
             else:
                 await bot.send_message(message.chat.id, 'Orqaga', reply_markup=user_uz)
 
+
         @dp.callback_query_handler(text='Xa')
         async def tel3(callback_query: types.CallbackQuery):
+            messa = callback_query.data
             user_id = callback_query.from_user.id
             date = str(datetime.datetime.today().date()) + "T00:00:00"
             params = {
@@ -229,131 +229,131 @@ async def uzb(message: types.Message):
                                                 message_id=callback_query.message.message_id,
                                                 reply_markup=None)
             
-    @dp.message_handler(text = 'Biz bilan bog’lanish')
-    async def admin(message: types.Message):
-        await message.answer("Admin: @pm_hilol")
+    # @dp.message_handler(text = 'Biz bilan bog’lanish')
+    # async def admin(message: types.Message):
+    #     await message.answer("Admin: @pm_hilol")
 
 
-    @dp.message_handler(text='Akt sverka olish') # qayta qayta bossa bo'lishi kk forga o'xshab
-    async def start_handler(message: types.Message, state: FSMContext):
-        params = {
-            "type": "contracts",
-            "chat_id": message.from_user.id
-        }
-        response = requests.get(api_url, params=params, headers=headers)
-        global ta, calback
-        ta =[]
-        calback = []
-        data = response.json()
-        print("ctla", data['contracts'])
-        buttons = []
-        for contract_info in data['contracts']:
-            button_text = str(contract_info['contract'])
-            cal_back = str(contract_info['contractID'])
-            ta.append(str(contract_info['contract']))
-            calback.append(str(contract_info['contractID']))
-            button = InlineKeyboardButton(text=button_text, callback_data=cal_back)
-            buttons.append([button])
-        hammasi = InlineKeyboardButton(text="Hammasi", callback_data="Hammasi")
-        orqaga = InlineKeyboardButton(text="Orqaga", callback_data="Orqaga")
-        buttons.append([hammasi])
-        ta.append('Hammasi')
-        calback.append('Hammasi')
-        buttons.append([orqaga])
-        ta.append('Orqaga')
-        calback.append('Orqaga')
+    # @dp.message_handler(text='Akt sverka olish') # qayta qayta bossa bo'lishi kk forga o'xshab
+    # async def start_handler(message: types.Message, state: FSMContext):
+    #     params = {
+    #         "type": "contracts",
+    #         "chat_id": message.from_user.id
+    #     }
+    #     response = requests.get(api_url, params=params, headers=headers)
+    #     global ta, calback
+    #     ta =[]
+    #     calback = []
+    #     data = response.json()
+    #     print("ctla", data['contracts'])
+    #     buttons = []
+    #     for contract_info in data['contracts']:
+    #         button_text = str(contract_info['contract'])
+    #         cal_back = str(contract_info['contractID'])
+    #         ta.append(str(contract_info['contract']))
+    #         calback.append(str(contract_info['contractID']))
+    #         button = InlineKeyboardButton(text=button_text, callback_data=cal_back)
+    #         buttons.append([button])
+    #     hammasi = InlineKeyboardButton(text="Hammasi", callback_data="Hammasi")
+    #     orqaga = InlineKeyboardButton(text="Orqaga", callback_data="Orqaga")
+    #     buttons.append([hammasi])
+    #     ta.append('Hammasi')
+    #     calback.append('Hammasi')
+    #     buttons.append([orqaga])
+    #     ta.append('Orqaga')
+    #     calback.append('Orqaga')
 
-        reply_markup = InlineKeyboardMarkup(inline_keyboard=buttons)
-        await message.answer("Shartnomalar", reply_markup=reply_markup)
-        await state.set_state("waiting_for_contract")
+    #     reply_markup = InlineKeyboardMarkup(inline_keyboard=buttons)
+    #     await message.answer("Shartnomalar", reply_markup=reply_markup)
+    #     await state.set_state("waiting_for_contract")
 
-    @dp.callback_query_handler(state = 'waiting_for_contract') # ishlasi kk to'xtivsz
-    async def act(callback_query: CallbackQuery, state: FSMContext):
-        global msgcall
-        msgcall = callback_query.data
-        print(msgcall)
-        if msgcall != "Orqaga":
-            await state.finish()
-            await TimeInput.start_time.set()
-            await callback_query.message.answer("Boshlang'ich sanani kiriting(masalan: Yil-OY-Kun): ")
-        elif msgcall == "Hammasi":
-            await state.finish()
-            await TimeInput.start_time.set()
-            await callback_query.message.answer("Boshlang'ich sanani kiriting(masalan: Yil-OY-Kun): ")
-        else:
-            await callback_query.message.edit_reply_markup(reply_markup=None)
-
-
-    @dp.message_handler(state=TimeInput.start_time)
-    async def start_time_handler(message: types.Message, state: FSMContext):
-        if re.match(r'\d{4}-\d{2}-\d{2}', message.text):
-            async with state.proxy() as data:
-                data['start'] = message.text
-                global start_t
-                start_t = message.text
-                await TimeInput.next()
-                await message.answer("Tugash sanani kiriting(masalan: Yil-OY-Kun): ")
-        else:
-            await message.answer("To'g'ri formatda sanani kiriting(masalan: Yil-OY-Kun): ")
+    # @dp.callback_query_handler(state = 'waiting_for_contract') # ishlasi kk to'xtivsz
+    # async def act(callback_query: CallbackQuery, state: FSMContext):
+    #     global msgcall
+    #     msgcall = callback_query.data
+    #     print(msgcall)
+    #     if msgcall != "Orqaga":
+    #         await state.finish()
+    #         await TimeInput.start_time.set()
+    #         await callback_query.message.answer("Boshlang'ich sanani kiriting(masalan: Yil-OY-Kun): ")
+    #     elif msgcall == "Hammasi":
+    #         await state.finish()
+    #         await TimeInput.start_time.set()
+    #         await callback_query.message.answer("Boshlang'ich sanani kiriting(masalan: Yil-OY-Kun): ")
+    #     else:
+    #         await callback_query.message.edit_reply_markup(reply_markup=None)
 
 
-    @dp.message_handler(state=TimeInput.end_time)
-    async def end_time_handler(message: types.Message, state: FSMContext):
-        if re.match(r'\d{4}-\d{2}-\d{2}', message.text):
-            async with state.proxy() as data:
-                data['finish'] = message.text
-                global start_t
-                start = start_t + 'T00:00:00'
-                finish = message.text + 'T00:00:00'
-                chat_id = message.from_user.id
-                if msgcall == 'Hammasi':
-                    if start_t < message.text:
-                        params = {
-                            "type": "reconciliation_act",
-                            "chat_id": chat_id,
-                            "start": start,
-                            "finish": finish
-                        }
-                if start_t < message.text:
-                    params = {
-                        "type": "reconciliation_act",
-                            "chat_id": chat_id,
-                            "start":start,
-                            "finish":finish,
-                            'contract_id':msgcall
-                    }
-                    response = requests.get(api_url, params=params, headers=headers)
-                    try:
-                        if response.ok:
-                            content_type = response.headers.get('Content-Type', '')
-                            if 'application/json' in content_type:
-                                data = response.json()
-                                if data.get('allsumm') is None:
-                                    await message.answer("Sizda hozircha mablag' yo ")
-                                else:
-                                    await message.answer(f"{data['allsumm']}----{data['contracts']}")
-                                await message.answer(f"Start time: {data['start']}, Finish time: {data['finish']}")
-                            elif 'application/octet-stream' in content_type or 'application/vnd.ms-excel' in content_type:
-                                bio = io.BytesIO(response.content)
-                                bio.name = 'received_file.xlsx'
-                                await message.answer_document(document=bio)
-                            else:
-                                result = f"Unknown Content-Type: {content_type}"
-                                await message.answer(f"Sizdagi xatolik {result}")
-                        else:
-                            result = f"Request failed with status code {response.status_code}: {response.reason}"
-                            # print("error", result)
-                            await message.answer(f"Sizdagi xatolik {result}")
+    # @dp.message_handler(state=TimeInput.start_time)
+    # async def start_time_handler(message: types.Message, state: FSMContext):
+    #     if re.match(r'\d{4}-\d{2}-\d{2}', message.text):
+    #         async with state.proxy() as data:
+    #             data['start'] = message.text
+    #             global start_t
+    #             start_t = message.text
+    #             await TimeInput.next()
+    #             await message.answer("Tugash sanani kiriting(masalan: Yil-OY-Kun): ")
+    #     else:
+    #         await message.answer("To'g'ri formatda sanani kiriting(masalan: Yil-OY-Kun): ")
 
-                    except requests.exceptions.RequestException as e:
-                        result = f"Request failed: {e}"
-                        await message.answer(f"Sizdagi xatolik exepdan {result}")
-                    await message.answer(f"Start time: {data['start']}, Finish time: {data['finish']}")
-                    await state.finish()
-                else:
-                    await message.answer("Boshlanish sanasi Tugash sanasidan  katta!")
-                    await TimeInput.start_time.set()
-                    await message.answer("Boshlang'ich sanani kiriting(masalan: Yil-OY-Kun): ")
+
+    # @dp.message_handler(state=TimeInput.end_time)
+    # async def end_time_handler(message: types.Message, state: FSMContext):
+    #     if re.match(r'\d{4}-\d{2}-\d{2}', message.text):
+    #         async with state.proxy() as data:
+    #             data['finish'] = message.text
+    #             global start_t
+    #             start = start_t + 'T00:00:00'
+    #             finish = message.text + 'T00:00:00'
+    #             chat_id = message.from_user.id
+    #             if msgcall == 'Hammasi':
+    #                 if start_t < message.text:
+    #                     params = {
+    #                         "type": "reconciliation_act",
+    #                         "chat_id": chat_id,
+    #                         "start": start,
+    #                         "finish": finish
+    #                     }
+    #             if start_t < message.text:
+    #                 params = {
+    #                     "type": "reconciliation_act",
+    #                         "chat_id": chat_id,
+    #                         "start":start,
+    #                         "finish":finish,
+    #                         'contract_id':msgcall
+    #                 }
+    #                 response = requests.get(api_url, params=params, headers=headers)
+    #                 try:
+    #                     if response.ok:
+    #                         content_type = response.headers.get('Content-Type', '')
+    #                         if 'application/json' in content_type:
+    #                             data = response.json()
+    #                             if data.get('allsumm') is None:
+    #                                 await message.answer("Sizda hozircha mablag' yo ")
+    #                             else:
+    #                                 await message.answer(f"{data['allsumm']}----{data['contracts']}")
+    #                             await message.answer(f"Start time: {data['start']}, Finish time: {data['finish']}")
+    #                         elif 'application/octet-stream' in content_type or 'application/vnd.ms-excel' in content_type:
+    #                             bio = io.BytesIO(response.content)
+    #                             bio.name = 'received_file.xlsx'
+    #                             await message.answer_document(document=bio)
+    #                         else:
+    #                             result = f"Unknown Content-Type: {content_type}"
+    #                             await message.answer(f"Sizdagi xatolik {result}")
+    #                     else:
+    #                         result = f"Request failed with status code {response.status_code}: {response.reason}"
+    #                         # print("error", result)
+    #                         await message.answer(f"Sizdagi xatolik {result}")
+
+    #                 except requests.exceptions.RequestException as e:
+    #                     result = f"Request failed: {e}"
+    #                     await message.answer(f"Sizdagi xatolik exepdan {result}")
+    #                 await message.answer(f"Start time: {data['start']}, Finish time: {data['finish']}")
+    #                 await state.finish()
+    #             else:
+    #                 await message.answer("Boshlanish sanasi Tugash sanasidan  katta!")
+    #                 await TimeInput.start_time.set()
+    #                 await message.answer("Boshlang'ich sanani kiriting(masalan: Yil-OY-Kun): ")
 
 
 
